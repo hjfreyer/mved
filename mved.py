65e9e211282bdacc5af4b6eaaaeca8749ec1a6f4
Hunter Freyer <yt@hjfreyer.com>
2011-01-15 19:47:33 -0500

    Rewrite. Does pretty much the same thing as before, but more concise. Also, allows you to specify a directory in which to mved.

--- mved.py
+++ mved.py
@@ -11,59 +11,102 @@
 
 import os
 import re
+import subprocess
 import sys
 import tempfile
 
 from optparse import OptionParser, OptionGroup
 
 
-def sorted_file_list(directory, all=False):
-  """Get a sorted list of the files in the given directory.
+def GetEditor():
+  """Try to get the user's editor from $EDITOR and $VISUAL.
 
-  Args:
-    all: if true, include hidden (dot) files.
+  If those aren't set, fall back on 'vi'.
   """
-  hidden = re.compile(r'^\.')
-  files = os.listdir(directory)
-
-  hidden = re.compile(r'^\.')
-  if not all:
-    new_files = []
-    for file in files:
-      if not hidden.match(file):
-        new_files.append(file)
-    files = new_files
-
-  files.sort()
-  return files
+  return os.getenv('EDITOR') or os.getenv('VISUAL') or 'vi'
 
 
-def get_editor():
-  """Try to get the user's editor from $EDITOR and $VISUAL. If those
-  aren't set, fall back on 'vi'."""
-  editor = os.getenv('EDITOR')
-  if editor is None:
-    editor = os.getenv('VISUAL')
-  if editor is None:
-    editor = 'vi'
-  return editor
+def PrintPendingAction(rename_pairs):
+  for old, new in rename_pairs:
+    if new:
+      print 'M %s -> %s' % (old, new)
+    else:
+      print 'D %s' % old
 
 
-def confirm(msg, default=False):
+def PrintWarnings(rename_pairs):
+  for old, new in rename_pairs:
+    if new.strip() != new:
+      print """
+WARNING: destination file "%s" has leading or trailing whitespace in its name.""" % new
+
+
+def Confirm():
   """Prompt the user with message to which they can reply 'y' or 'n'."""
+  conf = None
 
-  if default:
-    yesno = ' [Y/n] '
-  else:
-    yesno = ' [y/N] '
+  while conf not in ['y', 'n', '']:
+    conf = raw_input('Continue? [y/N] ').lower()
 
-  sys.stdout.write(msg + yesno)
-  response = sys.stdin.readline().strip().lower()
+  return conf == 'y'
 
-  if default:
-    return response != 'n' and response != 'no'
-  else:
-    return response == 'y' or response == 'yes'
+
+def DoRename(rename_pairs):
+  try:
+    for (old, new) in rename_pairs:
+      os.rename(old, new)
+  except OSError, e:
+    print 'ERROR: Failure during rename.'
+    raise
+
+
+def Mved(directory, list_all):
+  try:
+    os.chdir(directory)
+  except OSError, e:
+    print 'ERROR: Could not access directory: %s' % directory
+    exit(1)
+
+  files = [f for f in os.listdir(os.getcwd())
+           if list_all or (not f.startswith('.'))]
+  files.sort()
+
+  tmp = tempfile.NamedTemporaryFile()
+  tmp.write('\n'.join(files))
+  tmp.flush()
+
+  status = subprocess.call(GetEditor() + ' ' + tmp.name, shell=True)
+
+  if status:
+    print 'ERROR: Editor exited with nonzero status (%d).' % status
+    exit(status)
+
+  # Rewind file, get new names.
+  tmp.seek(0)
+  new_files = [f.rstrip('\n\r') for f in tmp.readlines()]
+
+  if len(files) != len(new_files):
+    print """\
+ERROR: You added or deleted a line. Don't do that. Use blank lines to
+delete files."""
+    exit(1)
+
+  rename_pairs = [(old, new) for (old, new) in zip(files, new_files)
+                  if old != new]
+
+  if not rename_pairs:
+    print 'Nothing to do.'
+    exit(0)
+
+  PrintPendingAction(rename_pairs)
+
+  PrintWarnings(rename_pairs)
+
+  if not Confirm():
+    print 'Aborting'
+    exit(0)
+
+  DoRename(rename_pairs)
 
 
 def main(argv):
@@ -78,76 +121,18 @@ def main(argv):
 
   (opts, args) = opt_parser.parse_args()
 
-  cwd = os.getcwd()
+  old_dir = os.getcwd()
+  directory = args and args[0] or old_dir
 
-  files = sorted_file_list(cwd, opts.list_all)
-
-  editor = get_editor()
-
-  (tmpfd, tmpname) = tempfile.mkstemp()
-  for file in files:
-    os.write(tmpfd, file + '\n')
-  os.close(tmpfd)
-
-  pid = os.fork()
-  if pid == 0:
-    # child
-    try:
-      os.execvp(editor, [editor, tmpname])
-    except RuntimeError, e:
-      print e
-      sys.exit(1)
-
-  # parent (child ends with execvp)
-  (w_pid, w_status) = os.waitpid(pid, 0)
-
-  if w_status != 0:
-    print '**** Editor exited with nonzero status (%d).' % w_status
-
-  tmp = open(tmpname, 'r')
-  i = 0
-  new_files = tmp.readlines()
-  for new_file in new_files:
-    old_file = files[i]
-    new_file = new_file.rstrip('\n\r')
-    if files[i] != new_file:
-      if new_file != '':
-        print 'RENAME: '%s' --> '%s'' % (old_file, new_file)
-      else:
-        print 'DELETE: '%s'' % old_file
-    i += 1
-  tmp.close()
-
-  os.unlink(tmpname)
-
-  if len(new_files) != len(files):
-    print 'ERROR: You added or deleted a line. Don\'t do that. Use blank lines to delete files.'
-    sys.exit(1)
-
-  response = confirm('Confirm changes?', default=(w_status == 0))
-  if response:
-    print 'Yes. Renaming...'
-  else:
-    print 'No. Aborting.'
-    sys.exit(0)
-
-  i = 0
-  while i < len(new_files):
-    old_file = files[i]
-    new_file = new_files[i].rstrip('\n\r')
-    if old_file != new_file:
-      try:
-        if new_file != '':
-          print 'RENAME: '%s' --> '%s'' % (old_file, new_file)
-          os.rename(old_file, new_file)
-        else:
-          print 'DELETE: '%s'' % old_file
-          os.unlink(old_file)
-      except OSError, e:
-        print e
-    i += 1
-
-  print 'Done.'
+  try:
+    Mved(directory, opts.list_all)
+  except SystemExit:
+    raise
+  except:
+    print 'ERROR: Unknown error.'
+    raise
+  finally:
+    os.chdir(old_dir)
 
 
 if __name__ == '__main__':
